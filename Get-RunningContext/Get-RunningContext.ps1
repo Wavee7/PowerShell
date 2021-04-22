9398be8d0d05bd70ba1a7b41698eb99b303f8d09
Wavee7 <79086831+Wavee7@users.noreply.github.com>
2021-04-22 17:30:14 +0200

    1.0.2

--- Get-RunningContext/Get-RunningContext.ps1
+++ Get-RunningContext/Get-RunningContext.ps1
@@ -1,9 +1,9 @@
 <#
 .SYNOPSIS
-    Get the current Windows running context
+    Get the current Windows running context on SMS and MDT
 
 .DESCRIPTION
-    This function will tell if we are running in FullOS, FullOS_OSD or WinPE context
+    This function will tell if we are running in FullOS, FullOS_OSD_SMS, FullOS_OSD_MDT or WinPE context
 
 .EXAMPLE
     # Get the current Windows running context of the script
@@ -15,6 +15,7 @@
     Contact  : @Wavee7
 
     Version history :
+    1.0.2 - (2021-04-21) - Added MDT detection and changed the output result to reflect if it's SMS or MDT
     1.0.1 - (2021-04-02) - Added FullOS_OSD so we can detect that we are on FullOS within the OSD
     1.0.0 - (2021-03-28) - Script created
 #>
@@ -25,7 +26,7 @@ function Get-RunningContext {
 
     .OUTPUTS
         Return a String with the current running context
-        ('FullOS', 'FullOS_OSD', 'WinPE')
+        ('FullOS', 'FullOS_OSD_SMS', 'FullOS_OSD_MDT', 'WinPE')
     #>
 
     [CmdletBinding()]
@@ -38,18 +39,20 @@ function Get-RunningContext {
     if ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name -like '*SYSTEM') {
         try {
             # Create an object to access the task sequence environment
-            $tsEnv = New-Object -ComObject 'Microsoft.SMS.TSEnvironment' -ErrorAction Stop
+            [System.MarshalByRefObject]$locSMBROTSEnv = New-Object -ComObject 'Microsoft.SMS.TSEnvironment' -ErrorAction Stop
 
-            # Check if we are running on WinPE
-            if ($tsEnv.Value('_SMSTSInWinPE') -eq $true) {
+            # Check if we are running on WinPE or FullOS
+            if ($locSMBROTSEnv.Value('_SMSTSInWinPE') -eq $true) {
                 $locStrCurrentContext = 'WinPE'
             }
             else {
-                $locStrCurrentContext = 'FullOS_OSD'
+                $locStrCurrentContext = 'FullOS_OSD_SMS'
             }
         }
         catch [System.Exception] {
-            Write-Warning -Message 'Unable to construct Microsoft.SMS.TSEnvironment object'
+            if (Test-Path -Path 'C:\MININT') {
+                $locStrCurrentContext = 'FullOS_OSD_MDT'
+            }
         }
     }
 
